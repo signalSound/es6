8fbc8c292d0db4d3ae1d4dbab88abc22c49cbf1e
e <1281292271@qq.com>
2020-09-17 15:34:54 +0800

    es6

---
+++ es6-babel/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+{
+    var PI = 3.14;
+
+    var sun = function sun(x, y) {
+        return x + y;
+    };
+
+    var _name$price = {
+        name: "es6",
+        price: "500"
+    },
+        name = _name$price.name,
+        price = _name$price.price;
+
+    var c = 7;
+    var d = "text";
+}
---
+++ es6-babel/src/index.js
@@ -0,0 +1,12 @@
+{
+    const PI = 3.14;
+
+    const sun = (x, y) => x + y
+
+    const {name, price} = {
+        name: "es6",
+        price: "500"
+    }
+    const c = 7
+    const d = "text"
+}
